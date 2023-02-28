6692b2e4dc339c2955d77b71f160a9245a436875
Emma Franzen-O'Quinn <efoquinn@proforma.com>
2023-02-28 13:14:42 -0500

    testing angularJS

--- ni.js
+++ ni.js
@@ -1,42 +1,36 @@
 const app = angular.module('emmaPage', ['ngRoute']);
 
-netlifyIdentity.on('init', () => {
-    initUser = netlifyIdentity.currentUser();
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'test.html'
+        })
+        .when("/home", {
+            templateUrl: 'test.html'
+        })
+        .when("/loggedIn", {
+            templateUrl: 'loggedIn.html'
+        })
+
+
 });
 
-netlifyIdentity.on('login', () => {
-    if (initUser == null) {
-        window.location.replace('loggedIn');
-    }
-    netlifyIdentity.close();
-});
+app.controller('efoNI', function ($scope) {
+    let initUser = netlifyIdentity.currentUser();
+    netlifyIdentity.on('init', () => {
+        initUser = netlifyIdentity.currentUser();
+    });
 
-netlifyIdentity.on('logout', () => {
-    netlifyIdentity.close();
-    window.location.replace('/');
-});
+    netlifyIdentity.on('login', () => {
+        if (initUser == null) {
+            window.location.replace('#!loggedIn');
+        }
+        netlifyIdentity.close();
+    });
 
+    netlifyIdentity.on('logout', () => {
+        netlifyIdentity.close();
+        window.location.replace('/');
+    });
 
-// app.config(function ($routeProvider) {
-//     $routeProvider
-//         .when("/", {
-//             templateUrl: 'index.html'
-//         })
-//         .when("/home", {
-//             templateUrl: 'index.html'
-//         })
-//         .when("/loggedIn", {
-//             templateUrl: 'loggedIn.html'
-//         })
-//         .when("/cats", {
-//             templateUrl: 'cats.html'
-//         })
-//         .when("/dogs", {
-//             templateUrl: 'dogs.html'
-//         });
-
-
-// });
-app.controller('efoNI', function () {
-   
 });
