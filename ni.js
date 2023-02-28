72c17bff78a02ef74e891d7bdcf6a4b4abb06222
Emma Franzen-O'Quinn <efoquinn@proforma.com>
2023-02-28 10:35:49 -0500

    angularJS

--- ni.js
+++ ni.js
@@ -13,3 +13,24 @@ netlifyIdentity.on('logout', () => {
     netlifyIdentity.close();
     window.location.replace('/');
 });
+
+const app = angular.module('emmaPage', ['ngRoute']);
+
+app.config(function ($routeProvider){
+    $routeProvider
+    .when("/", {
+        templateUrl: 'index.html'
+    })
+    .when("/home", {
+        templateUrl: 'index.html'
+    })
+    .when("/cats", {
+        templateUrl: 'cats.html'
+    })
+    .when("/dogs", {
+        templateUrl: 'dogs.html'
+    })
+
+app.controller('efoNI', function())
+
+    });
