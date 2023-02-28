3773afc0a5dc13f94d3a833153918fdc40adf721
Emma Franzen-O'Quinn <efoquinn@proforma.com>
2023-02-28 11:07:02 -0500

    js controller

--- ni.js
+++ ni.js
@@ -35,7 +35,7 @@ app.config(function ($routeProvider) {
             templateUrl: 'dogs.html'
         })
 
-    app.controller('efoNI', function () {
+    app.controller('efoNI', function ($scope, $http) {
 
     });
 
